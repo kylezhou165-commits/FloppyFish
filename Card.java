df31aa63b3326bd354fd4da485c3b095a4a12be7
Michael <donutaskwhy787@gmail.com>
2026-04-08 14:55:51 -0400

    Refactor Flash class to Card with updated methods

--- Card.java
+++ Card.java
@@ -1,90 +1,31 @@
-import java.util.*;
-
-public class Flash 
+public class Card
 {
-    public ArrayList<Card> arr;
-    public LinkedList<Card> next;
+    public String value;
+    public String answer;
     
-    public Flash()
+    public Card()
     {
-        arr = new ArrayList<Card>();
-        next = new LinkedList<Card>();
+        value = "";
+        answer = "";
     }
     
-    public void add(String s, String v)
+    public Card(String v, String s)
     {
-        Card tmp = new Card(s, v);
-        
-        arr.add(tmp);
-        next.add(tmp);
+        value = v;
+        answer = s;
+    }
+    
+    public boolean equals(Card c)
+    {
+        if(!value.equals(c.value))
+            return false;
+        if(!answer.equals(c.answer))
+            return false;
+        return true;
     }
     
     public String toString()
     {
-        String str = arr.toString() + "\n" + next.toString();
-        return str;
+        return value;
     }
-    
-    public void remove(int i)
-    {
-        
-        Card tmp = arr.remove(i);
-        ListIterator<Card> it = next.listIterator();
-        
-        while(it.hasNext())
-        {
-            Card g = it.next();
-            if(g.equals(tmp))
-                it.remove();
-        }
-    }
-    
-    public Card nextCard()
-    {
-        Card tmp = next.getFirst();
-        next.removeFirst();
-        next.addLast(tmp);
-        return tmp;
-    }
-    
-    public void shuffle()
-    {
-        shuffle(200);
-    }
-    
-    public void shuffle(int n)
-    {
-        next = new LinkedList<Card>();
-        ArrayList<Card> copy = new ArrayList<Card>();
-        
-        for(Card c : arr)
-            copy.add(c);
-            
-        stupidShuffle(copy, n);
-        
-        for(Card c : copy)
-        {
-            next.add(c);
-        }
-    }
-    
-    private static void stupidShuffle(ArrayList arrTmp, int n)
-    {
-        for(int i = 0; i < n; i++)
-        {
- 
-            for(int ind = 0; ind < arrTmp.size() - 1; ind++)
-            {
-                if(Math.random() < 0.5)
-                {
-                   
-                    Object tmp = arrTmp.get(ind + 1);
-                    arrTmp.set(ind + 1, arrTmp.get(ind));
-                    arrTmp.set(ind, tmp);
-                }
-            }
-        }
-    }
-    
-    
 }
