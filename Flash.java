0a1863368d5436bd3815b916edc67e92253a3e28
Michael <donutaskwhy787@gmail.com>
2026-04-08 14:55:03 -0400

    uh fixxed everything

--- Flash.java
+++ Flash.java
@@ -11,7 +11,7 @@ public class Flash
         next = new LinkedList<Card>();
     }
     
-    public void addCard(String s, String v)
+    public void add(String s, String v)
     {
         Card tmp = new Card(s, v);
         
@@ -19,18 +19,24 @@ public class Flash
         next.add(tmp);
     }
     
-    public Card removeCard(int i)
+    public String toString()
+    {
+        String str = arr.toString() + "\n" + next.toString();
+        return str;
+    }
+    
+    public void remove(int i)
     {
         
-        Card tmp = arr.remove(i)
-        Iterator<Card> it = next.iterator();
+        Card tmp = arr.remove(i);
+        ListIterator<Card> it = next.listIterator();
         
         while(it.hasNext())
         {
-            if(it.next().equals(tmp))
-                return it.remove();
+            Card g = it.next();
+            if(g.equals(tmp))
+                it.remove();
         }
-        
     }
     
     public Card nextCard()
@@ -43,8 +49,42 @@ public class Flash
     
     public void shuffle()
     {
-        next = new LinkedList<Card>();
-        
+        shuffle(200);
     }
+    
+    public void shuffle(int n)
+    {
+        next = new LinkedList<Card>();
+        ArrayList<Card> copy = new ArrayList<Card>();
+        
+        for(Card c : arr)
+            copy.add(c);
+            
+        stupidShuffle(copy, n);
+        
+        for(Card c : copy)
+        {
+            next.add(c);
+        }
+    }
+    
+    private static void stupidShuffle(ArrayList arrTmp, int n)
+    {
+        for(int i = 0; i < n; i++)
+        {
+ 
+            for(int ind = 0; ind < arrTmp.size() - 1; ind++)
+            {
+                if(Math.random() < 0.5)
+                {
+                   
+                    Object tmp = arrTmp.get(ind + 1);
+                    arrTmp.set(ind + 1, arrTmp.get(ind));
+                    arrTmp.set(ind, tmp);
+                }
+            }
+        }
+    }
+    
     
 }
