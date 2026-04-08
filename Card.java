07289fba7cf6e4fdc17b9c8828039f239eead22c
Michael <donutaskwhy787@gmail.com>
2026-04-08 14:55:26 -0400

    Refactor Card class into Flash class with new methods

--- Card.java
+++ Card.java
@@ -1,26 +1,90 @@
-public class Card
+import java.util.*;
+
+public class Flash 
 {
-    public String value;
-    public String answer;
+    public ArrayList<Card> arr;
+    public LinkedList<Card> next;
     
-    public Card()
+    public Flash()
     {
-        value = "";
-        answer = "";
+        arr = new ArrayList<Card>();
+        next = new LinkedList<Card>();
     }
     
-    public Card(String v, String s)
+    public void add(String s, String v)
     {
-        value = v;
-        answer = s;
+        Card tmp = new Card(s, v);
+        
+        arr.add(tmp);
+        next.add(tmp);
     }
     
-    public boolean equals(Card c)
+    public String toString()
     {
-        if(!value.equals(c.value))
-            return false;
-        if(!answer.equals(c.answer))
-            return false;
-        return true;
+        String str = arr.toString() + "\n" + next.toString();
+        return str;
     }
+    
+    public void remove(int i)
+    {
+        
+        Card tmp = arr.remove(i);
+        ListIterator<Card> it = next.listIterator();
+        
+        while(it.hasNext())
+        {
+            Card g = it.next();
+            if(g.equals(tmp))
+                it.remove();
+        }
+    }
+    
+    public Card nextCard()
+    {
+        Card tmp = next.getFirst();
+        next.removeFirst();
+        next.addLast(tmp);
+        return tmp;
+    }
+    
+    public void shuffle()
+    {
+        shuffle(200);
+    }
+    
+    public void shuffle(int n)
+    {
+        next = new LinkedList<Card>();
+        ArrayList<Card> copy = new ArrayList<Card>();
+        
+        for(Card c : arr)
+            copy.add(c);
+            
+        stupidShuffle(copy, n);
+        
+        for(Card c : copy)
+        {
+            next.add(c);
+        }
+    }
+    
+    private static void stupidShuffle(ArrayList arrTmp, int n)
+    {
+        for(int i = 0; i < n; i++)
+        {
+ 
+            for(int ind = 0; ind < arrTmp.size() - 1; ind++)
+            {
+                if(Math.random() < 0.5)
+                {
+                   
+                    Object tmp = arrTmp.get(ind + 1);
+                    arrTmp.set(ind + 1, arrTmp.get(ind));
+                    arrTmp.set(ind, tmp);
+                }
+            }
+        }
+    }
+    
+    
 }
