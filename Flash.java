f585822bf80b982025792671b1dc5c03ed7e7107
Michael <donutaskwhy787@gmail.com>
2026-04-30 10:12:18 -0400

    Update Flash.java

--- Flash.java
+++ Flash.java
@@ -84,36 +84,4 @@ public class Flash
         arr.set(i, tmp.get(i));
        }
     }
-    
-    public static class Card
-{
-    public String value;
-    public String answer;
-    
-    public Card()
-    {
-        value = "";
-        answer = "";
-    }
-    
-    public Card(String v, String s)
-    {
-        value = v;
-        answer = s;
-    }
-    
-    public boolean equals(Card c)
-    {
-        if(!value.equals(c.value))
-            return false;
-        if(!answer.equals(c.answer))
-            return false;
-        return true;
-    }
-    
-    public String toString()
-    {
-        return value;
-    }
-}
 }
