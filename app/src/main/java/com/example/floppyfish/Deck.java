512f8906b7e9a5b9172e0a45b1ac1cec1cf369fa
kylezhou165-commits <kylezhou165@gmail.com>
2026-05-28 12:47:30 -0400

    Deck.java Fixed boilerplate I forgot
    Fixed Database

--- app/src/main/java/com/example/floppyfish/Deck.java
+++ app/src/main/java/com/example/floppyfish/Deck.java
@@ -1,8 +1,19 @@
 /*
+package com.FloppyFish;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
 @Entity 
 public class Deck
 {
-  @PrimaryKey
+  @PrimaryKey (autoGenerate = true)
   public int deckNumber; 
+
+  public Deck() {}
+
+  public Deck(int deckNumber)
+  {
+    this.deckNumber = deckNumber;
+  }
 }
 */
