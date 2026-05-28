72bb7db77bba7f42a556e04b1a61c5304bfb5816
kylezhou165-commits <kylezhou165@gmail.com>
2026-05-28 13:12:44 -0400

    Key database class

---
+++ app/src/main/java/com/example/floppyfish/QuizletDatabase.java
@@ -0,0 +1,7 @@
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Deck.class, Flashcard.class}, version = 1)
+public abstract class QuizletDatabase extends RoomDatabase {
+    public abstract DecksWithCards decksWithCards();
+}
