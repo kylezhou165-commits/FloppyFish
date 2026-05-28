bfecc54b39821a113d60b612196c6c36bf948417
kylezhou165-commits <kylezhou165@gmail.com>
2026-05-28 11:48:20 -0400

    Create decksWithCards.java
    Relation class for one to many mapping for database

---
+++ app/src/main/java/com/example/floppyfish/decksWithCards.java
@@ -0,0 +1,13 @@
+/*
+public class UserWithBooks 
+{
+    @Embedded 
+    public User deck;
+
+    @Relation(
+        parentColumn = "deckNumber",    
+        entityColumn = "deckContainer"    
+    )
+    public List<Flashcard> Flashcards;
+}
+*/
