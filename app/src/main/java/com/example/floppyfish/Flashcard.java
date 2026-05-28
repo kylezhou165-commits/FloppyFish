8a8eb481a23383efbbb565027012aa1144eaaf0e
kylezhou165-commits <kylezhou165@gmail.com>
2026-05-28 10:12:33 -0400

    Create Flashcard.java
    Entity for flashcard

---
+++ app/src/main/java/com/example/floppyfish/Flashcard.java
@@ -0,0 +1,16 @@
+/*
+@Entity 
+public class Flashcard
+{
+  @PrimaryKey
+  public int deckContainer; 
+  public int cardNumber;
+  
+
+  @ColumnInfo(name = "value")
+  public String value;
+
+  @ColumnInfo(name = "answer")
+  public String answer;
+}
+*/
